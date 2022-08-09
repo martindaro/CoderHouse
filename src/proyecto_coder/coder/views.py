01956f2ad699478e9efdd6212a169517d89104b9
Martín Rodríguez <martin.darodriguez@gmail.com>
2022-08-09 20:45:57 -0300

    Aplicaciones y Modelos

--- src/proyecto_coder/coder/views.py
+++ src/proyecto_coder/coder/views.py
@@ -1,9 +1,23 @@
 
+from http.client import HTTPResponse
+from urllib.request import HTTPErrorProcessor
 from django.shortcuts import render
 from django.http import HttpResponse
-from coder.models import Curso
+from coder.models import Curso, Estudiante, Profesor, Entregable
 
 # Create your views here.
+
+def inicio(request):
+    return render(request, "coder/index.html", {"mensaje": "este es un mensaje de prueba"}) #esto renderiza el template de una
+
+def estudiante(request):
+    return HttpResponse("Vista de estudiante")
+
+def profesor(request):
+    return HttpResponse("Vista de profesor")
+
+def entregable(request):
+    return HttpResponse("Vista de entregable")
 
 def lista_cursos(request):
     cursos = Curso.objects.all()
@@ -13,4 +27,3 @@ def lista_cursos(request):
         lista_cursos_nombres.append(curso.nombre)
 
     return HttpResponse(lista_cursos_nombres)
-
